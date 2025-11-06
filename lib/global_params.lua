c34ace0ca1e28ddda1b145b005b972fae052fc0d
Chris Farrell <chris@thefarrells.nyc>
2025-11-06 07:06:06 +0000

    Nov 5, 2025, 11:06 PM

--- lib/global_params.lua
+++ lib/global_params.lua
@@ -3,9 +3,7 @@
 local GlobalParams = {}
 
 function GlobalParams.setup()
-  params:add_separator("GLOBAL")
-  params:add { type = "number", id = "max_drift", name = "max phrase drift", min = 1, max = 10, default = 3 }
-  params:add { type = "number", id = "repeat_probability", name = "repeat probability", min = 0, max = 10, default = 5 }
+  params:add_separator("PHRASING")
   params:add { type = "option", id = "grace_len_beats", name = "grace time", options = { "1/64", "1/32", "1/16" }, default = 2 }
 end
 
