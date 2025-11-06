bc30c44c025d5ce60e7d3cfb3e42fba5bf13d974
Chris Farrell <chris@thefarrells.nyc>
2025-11-06 01:27:49 +0000

    Nov 5, 2025, 5:27 PM

---
+++ lib/global_params.lua
@@ -0,0 +1,14 @@
+-- Global parameters for Get in the Sea
+
+local GlobalParams = {}
+
+function GlobalParams.setup()
+  params:add_separator("GLOBAL")
+  params:add { type = "number", id = "max_drift", name = "max phrase drift", min = 1, max = 10, default = 3 }
+  params:add { type = "number", id = "repeat_probability", name = "repeat probability", min = 0, max = 10, default = 5 }
+  params:add { type = "control", id = "grace_len_beats", name = "grace length (beats)", controlspec = controlspec.new(0.0625, 0.5, 'lin', 0, 0.0625, 'beats') }
+end
+
+return GlobalParams
+
+
